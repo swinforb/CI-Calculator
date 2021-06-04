4455c80f1ccf52bb7a506e9638a3306e8bb43729
swinforb <67295128+swinforb@users.noreply.github.com>
2021-06-04 09:53:54 -0700

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -4,10 +4,19 @@ Tests for calc app
 
 import calculator
 
-
+"""
 class TestCalculatorApp:
 	def test_add(self):
 		assert 5 == calculator.add(3, 2)
 
 	def test_subtract(self):
 		assert 5 == calculator.subtract(10, 5)
+"""
+
+class TestCalculatorApp:
+
+    def test_add(self):
+        assert 5 == calculator.add(1, 4)
+
+    def test_subtract(self):
+        assert 2 == calculator.subtract(5, 3)
