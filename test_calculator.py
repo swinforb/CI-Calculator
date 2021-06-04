0e42dd5c94a730dfb8f6a7db7553703a061c78ee
swinforb <67295128+swinforb@users.noreply.github.com>
2021-06-04 11:46:09 -0700

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -12,3 +12,9 @@ class TestCalculatorApp:
 
     def test_subtract(self):
         assert 2 == calculator.subtract(5, 3)
+
+    def test_multiply(self):
+        assert 6 == calculator.multiply(2, 3)
+
+    def test_divide(self):
+        assert 3 == calculator.divide(9, 3)
