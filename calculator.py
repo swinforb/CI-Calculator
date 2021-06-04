fb4fa777701c41c4bb95c7b72c136ac8a06a4979
swinforb <67295128+swinforb@users.noreply.github.com>
2021-06-04 09:47:19 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,9 +1,23 @@
 """
 calculator app using circle ci
 """
+"""
 def add(a, b):
   return a + b
 
 
 def subtract(a, b):
   return a - b
+"""
+
+"""
+Calculator app
+"""
+
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
